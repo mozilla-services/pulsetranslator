98d7cbf6974188ad7f4c81980d46e3a0ed737401
Jonathan Griffin <jgriffin@mozilla.com>
2015-10-14 17:13:02 -0700

    Turn on SSL

--- pulsetranslator/loghandler.py
+++ pulsetranslator/loghandler.py
@@ -11,7 +11,7 @@ import requests
 from mozillapulse.publishers import NormalizedBuildPublisher
 
 from translatorexceptions import LogTimeoutError
-from translatorqueues import publish_message
+from .translatorqueues import publish_message
 
 DEBUG = False
 
